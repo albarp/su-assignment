23ffedd369401837deb745999548dc7878273473
Alberto Arpini <alberto.arpini@gmail.com>
2025-05-24 14:47:19 +0000

    added new request object Order

---
+++ src/PurchaseCart.Domain/Requests/OrderRequest.cs
@@ -0,0 +1,17 @@
+namespace PurchaseCart.Domain.Requests;
+
+public class OrderRequest
+{
+    public Order Order { get; set; } = null!;
+}
+
+public class Order
+{
+    public List<OrderItem> Items { get; set; } = new();
+}
+
+public class OrderItem
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+} 
